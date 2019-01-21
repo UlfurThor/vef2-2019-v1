f4d7f6816812d25e9f0f45106d9699ff36c0b099
UlfurThor <ulfurthor@gmail.com>
2019-01-21 05:13:16 +0000

    started

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-/* todo, setja upp express */
+/*
+Keyrt með:
+node 09.template-ejs.js
+
+Keyrir upp vef með express sem notar ejs template fyrir síður á / og /about
+*/
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Þetta verður aðgengilegt gegnum `local.bar` í template
+app.locals.foo = bar => `foo ${bar}!`;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'public/views'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+  console.log('1');
+  // `title` verður aðgengilegt sem breyta í template
+  res.render('index', { title: 'Forsíða' });
+});
+
+app.get('/about', (req, res) => {
+  console.log('2');
+  const staff = ['Jón', 'Gunna'];
+  const extra = '<p><strong>Þessi síða er í vinnslu</strong></p>';
+
+  // Getum sent eins mikið og við viljum af gögnum til template gegnum hlut
+  res.render('about', { title: 'Um', staff, extra });
+});
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
