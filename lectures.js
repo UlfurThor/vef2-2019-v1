a745ff00eb515506cf068d8df5ee7b6b0e137bfd
UlfurThor <ulfurthor@gmail.com>
2019-01-23 03:02:22 +0000

    list renders

--- lectures.js
+++ lectures.js
@@ -20,6 +20,75 @@ function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
+function el(name, ...children) {
+  const element = document.createElement(name);
+
+  if (Array.isArray(children)) {
+    for (let child of children) { /* eslint-disable-line */
+      if (typeof child === 'string') {
+        element.appendChild(document.createTextNode(child));
+      } else if (child) {
+        element.appendChild(child);
+      }
+    }
+  }
+
+  return element;
+}
+
+
+function renderItem(data) {
+  const image = el('div');
+  image.classList.add('listItem__image');
+
+  if (data.thumbnail) {
+    const img = el('img');
+    img.setAttribute('src', data.thumbnail);
+    img.setAttribute('alt', '');
+    image.appendChild(img);
+  } else {
+    image.classList.add('listItem__image--empty');
+  }
+
+  const category = el('span', data.category);
+  category.classList.add('listItem__category');
+
+  const heading = el('h2', data.title);
+  heading.classList.add('listItem__title');
+
+  const textElements = el('div', category, heading);
+  textElements.classList.add('listItem__texts');
+
+  const text = el('div', textElements);
+  text.classList.add('listItem__bottom');
+
+  if (data.finished) {
+    const finished = el('div', '✓');
+    finished.classList.add('listItem__finished');
+    text.appendChild(finished);
+  }
+
+  const item = el('a', image, text);
+  item.classList.add('listItem');
+  item.setAttribute('href', `${data.slug}`);
+
+  return item;
+}
+
+
+function renderLectures(data) {
+  const items = data.map((item) => {
+    const col = el('div', renderItem(item));
+    col.classList.add('list__col');
+    return col;
+  });
+
+  const row = el('div', ...items);
+  row.classList.add('list__row');
+  return row;
+  // this.setContent(row);
+}
+
 
 const filepath = 'lectures.json';
 
@@ -29,24 +98,24 @@ async function list(req, res) {
   const dataRaw = await readFileAsync(filepath);
   const data = JSON.parse(dataRaw.toString('utf8')).lectures;
 
-  const wrapper = document.createElement('div');
+  const wrapper = renderLectures(data);
+  /*
+    // jsdom útfærir DOM staðal
+    wrapper.classList.add('reciepe');
+    wrapper.dataset.foo = 'bar';
 
-  // jsdom útfærir DOM staðal
-  wrapper.classList.add('reciepe');
-  wrapper.dataset.foo = 'bar';
+    const ul = document.createElement('ol');
 
-  const ul = document.createElement('ol');
+    console.log('asdasd');
+    data.forEach((lect) => {
+      console.log(lect.slug);
+      const li = document.createElement('li');
+      li.appendChild(document.createTextNode(lect.slug));
+      ul.appendChild(li);
+    });
 
-  console.log('asdasd');
-  data.forEach((lect) => {
-    console.log(lect.slug);
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(lect.slug));
-    ul.appendChild(li);
-  });
-
-  wrapper.appendChild(ul);
-
+    wrapper.appendChild(ul);
+  */
   const sluglist = data.map((lect) => {
     return lect.slug;
   });
@@ -59,7 +128,7 @@ async function list(req, res) {
     title: 'Forsíða',
     header_type: 'heading--main',
     page_type: 'index',
-    lectures: sluglist,
+    lectures: wrapper.outerHTML,
   });
 }
 
